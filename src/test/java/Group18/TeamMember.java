deedc2425690508f61513395b223fa5aa74ad2b6
reshoCakir <70242532+reshoCakir@users.noreply.github.com>
2020-11-01 15:11:45 -0500

    added team member

---
+++ src/test/java/Group18/TeamMember.java
@@ -0,0 +1,18 @@
+package Group18;
+
+public class TeamMember {
+
+    // Adem
+    // Festina
+    //Resho
+    //Roman
+    //Bekir
+    //Gulhanim
+    //Natalia
+    //Flip
+    //Subi
+    //Arpat
+    //Marat
+    //Ahmet
+
+}
